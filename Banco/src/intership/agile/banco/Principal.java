f0a5322139e07f7350e9426af2790b868b5bcf37
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-08 13:22:59 +0100

    Cambio de la clase principal a un paquete

---
+++ Banco/src/intership/agile/banco/Principal.java
@@ -0,0 +1,12 @@
+package intership.agile.banco;
+
+public class Principal {
+    // Buena práctica de programación: Almacenar nombres de llaves en constantes estáticas
+    private static final String PROP_USERNAME = "internship.agile.banco.username";
+    private static final String PROP_PASSWORD = "internship.agile.banco.password";
+    private static final byte TRY_LIMIT = 3;
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
